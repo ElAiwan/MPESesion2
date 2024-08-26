cdb4e2114158b84497279620223f66eaf992db2f
ElAiwan <chavarriaivan97@gmail.com>
2024-08-26 11:00:16 -0600

    sesion 26-08-2024

---
+++ Sesion3/Program.cs
@@ -0,0 +1,26 @@
+﻿using Sesion3.modelos;
+
+namespace Sesion3
+{
+    internal class Program
+    {
+        /* Calcular la edad de una persona y decir si es mayor o menor de edad */
+
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            string name;
+            DateTime fechaNac;
+
+            Console.WriteLine("Nombre: ");
+            name = Console.ReadLine();
+            
+            Console.WriteLine("Fecha de nacimiento: ");
+            fechaNac = DateTime.Parse(Console.ReadLine());  // Utilizar plecas "/" para las fechas. Ej.: 25/08/1998
+
+            persona.Name = name;
+            persona.FechaNac = fechaNac;
+            Console.WriteLine(persona.EvaluarEdad());
+        }
+    }
+}
---
+++ Sesion3/modelos/Persona.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion3.modelos
+{
+    internal class Persona
+    {
+        public Persona() { }
+
+        public string Name { get; set; }
+
+        public DateTime FechaNac { get; set; }
+
+        public int CalcularEdad()
+        {
+            // return DateTime.Now.Year - FechaNac.Year;
+            int edad = 0;
+            try     // intentar lo que está dentro de la llave
+            {
+                edad = DateTime.Now.Year - FechaNac.Year;
+            }
+            catch (Exception ex)    // cachar excepción de dato ingresado
+            {
+                Console.WriteLine($"Ocurrió un error: {ex.Message}");
+            }
+            return edad;
+        }
+
+        public string EvaluarEdad()
+        {
+            if (CalcularEdad() >= 18) return "Mayor de edad";
+            return "Menor de edad";
+        }
+    }
+}
